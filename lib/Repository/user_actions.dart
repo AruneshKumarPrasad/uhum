895d9039844ae6dde9c90ed6d257c2c821c947e7
Riad Haidar <riadhaidar666@gmail.com>
2023-03-15 06:48:23 -0400

    new actions file

---
+++ lib/Repository/user_actions.dart
@@ -0,0 +1,63 @@
+
+
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserActions{
+  // Profile Image Modification
+  final ImagePicker _profileImagepicker = ImagePicker();
+  File? profileImage;
+  bool isPimageLoaded = false;
+
+  Future<void> getPorfileImage() async {
+    final pickedFile = await _profileImagepicker.pickImage(
+      source: ImageSource.gallery,
+    );
+    if (pickedFile != null) {
+      profileImage = File(pickedFile.path);
+      //  loadedProfileImage =  File(profileImage.path);
+      //  print(pickedFile.path);
+      uploadProfileImage();
+      isPimageLoaded = true;
+    } else {
+      print('No Image Selected');
+    }
+  }
+void uploadProfileImage() {
+    FirebaseStorage.instance
+        .ref()
+        .child('users/${Uri.file(profileImage!.path).pathSegments.last}')
+        .putFile(File(profileImage!.path))
+        .then((value) {
+      value.ref.getDownloadURL().then((value) {
+        print(value);
+        //      getUserData();
+       // updateProfileImage(profileImage: value);
+      }).catchError((error) {
+        print('my error is: ');
+
+        print(error.toString());
+      });
+    }).catchError((error) {
+      print('error is: ');
+      print(error.toString());
+      // print(error.toString());
+    });
+  }
+
+    void updateProfileImage({required String profileImage,required String uId}) {
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(uId)
+        .update({"image": profileImage}).then((value) {
+      //getUserData();
+    }).catchError((error) {
+      print(error.toString());
+    });
+  }
+
+
+}
